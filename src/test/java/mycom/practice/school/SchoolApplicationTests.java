517b5d7eddc0589209d37e2249aaf2dab3e67ed1
Jha <397813@cognizant.com>
2022-04-24 10:58:29 +0200

    schoolmanagement base code commit

---
+++ src/test/java/mycom/practice/school/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package mycom.practice.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
